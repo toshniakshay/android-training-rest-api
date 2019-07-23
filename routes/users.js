48ad27960ccde905568a8c0414a4699a679cac43
akshay toshniwal <toshniwalakshay2@gmail.com>
2019-07-23 22:14:43 +0530

    Added CRUD functionality for user API

--- routes/users.js
+++ routes/users.js
@@ -5,19 +5,62 @@ const router = express.Router()
 const users = []
 
 router.get('', (req, res) => {
-    res.send("Api call successful")
+    res.status(200).send(users)
+})
+
+// lodash library : how to remove perticular property from object 
+router.get('/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id))
+
+    if(!user) return res.status(404).send("No user found.")
+
+    res.status(200).send(user)
 })
 
 router.post('', (req, res) => {
+    const user = {
+        id : users.length + 1,
+        first_name : req.body.first_name,
+        last_name : req.body.last_name,
+        gender : req.body.gender,
+        country : req.body.country,
+        state : req.body.state,
+        email : req.body.email,
+        password : req.body.password
+    }
+
+    users.push(user)
+
+    res.status(201).send(user)
+})
+
+router.put('/:id', (req, res) => {
+
+    const user = users.find(u => u.id === parseInt(req.params.id))
+
+    if(!user) return res.status(404).send("No user found.")
+
+    user.first_name = req.body.first_name
+    user.last_name = req.body.last_name
+    user.gender = req.body.gender
+    user.country = req.body.country
+    user.state = req.body.state
+    user.email = req.body.email
+    user.password = req.body.password
+
+    res.status(200).send(user)
 
 })
 
-router.put('', (req, res) => {
+// slice vs splice
+router.delete('/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id))
 
-})
+    if(!user) return res.status(400).send("User not found")
 
-router.delete('', (req, res) => {
+    users.splice(user, 1)
 
+    res.status(200).send("User deleted sucessfully")
 })
 
 module.exports = router
